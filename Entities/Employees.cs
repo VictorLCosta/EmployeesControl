2bd5d20e36f1b065c89c59917045cfbd9f94ebbd
VictorLCosta <victorlc2019@outlook.com>
2020-04-15 13:29:39 -0300

    Class Employees created

---
+++ Entities/Employees.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace EmployeesControl.Entities
+{
+    class Employees
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public double Salary { get; private set; }
+
+        public Employees()
+        {
+        }
+
+        public Employees(int id, string name, string email, double salary)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, {Name}, {Email}, R$ {Salary.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
